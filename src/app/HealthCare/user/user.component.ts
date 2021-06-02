c8531d53e68ef62f2a29e73215383cc3dddb17cf
afreenunnisa <afreenunnisa8686@gmail.com>
2021-06-02 11:26:02 +0530

    first commit

--- src/app/HealthCare/user/user.component.ts
+++ src/app/HealthCare/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/Model/user';
+import { LoginService } from 'src/app/Services/login.service';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  users!: User[];
+  presentUser!: User;
+  updateReq = false;
+  constructor(
+    private userServ: UserService,
+    private logServ: LoginService,
+    private routes: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    let users = this.getAllUsers();
+    if (this.logServ.Role == 'user') {
+      this.updateReq = true;
+    }
   }
 
+  getAllUsers() {
+    return this.userServ.getAll().subscribe(
+      (data) => {
+        this.users = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  deleteUser(id: number) {
+    this.userServ.deleteUser(id).subscribe(
+      (data) => {
+        this.getAllUsers();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  update(index: number) {
+    this.updateReq = true;
+    this.presentUser = this.users[index];
+  }
+
+  childEvent(event: any) {
+    if (this.logServ.Role == 'user') this.routes.navigateByUrl('login');
+    else this.updateReq = false;
+    this.getAllUsers();
+  }
 }
--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,9 +1,57 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError, shareReplay } from 'rxjs/operators';
+import { User } from '../Model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAll() {
+    return this.http
+      .get<User[]>('http://localhost:8888/user/getAll')
+      .pipe(catchError(this.handleError), shareReplay());
+  }
+
+  updateUser(user: User) {
+    return this.http
+      .put<User>('http://localhost:8888/user/updateUser', {
+        username: user.username,
+        password: user.password,
+        userid: user.id,
+        role: user.role,
+      })
+      .pipe(catchError(this.handleError), shareReplay());
+  }
+
+  registerAdmin(user: User) {
+    return this.http
+      .post<User>('http://localhost:8888/admin/registeradmin', {
+        username: user.username,
+        password: user.password,
+        role: 'ADMIN',
+      })
+      .pipe(catchError(this.handleError), shareReplay());
+  }
+  registerUser(user: User) {
+    return this.http
+      .post<User>('http://localhost:8888/user/adduser', {
+        username: user.username,
+        password: user.password,
+        role: 'user',
+      })
+      .pipe(catchError(this.handleError), shareReplay());
+  }
+
+  deleteUser(id: number) {
+    return this.http
+      .delete<User>('http://localhost:8888/admin/deleteAdmin/' + id)
+      .pipe(catchError(this.handleError), shareReplay());
+  }
+  handleError(error: HttpErrorResponse) {
+    return throwError(error.message);
+  }
 }
